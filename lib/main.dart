536366ed1b64fc82e45f8de60b9310bd347865b0
rui-watanabe <wataru610806@gmail.com>
2023-12-29 16:57:32 +0900

    遷移の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_roulette_app/roulette.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:roulette/roulette.dart';
@@ -25,83 +26,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late RouletteController _controller;
-
-  @override
-  // StatefulWidgetのみ
-  void initState() {
-    // RouletteControllerを初期化
-    _controller = RouletteController(
-        group: RouletteGroup([
-          const RouletteUnit.text("A", color: Colors.red),
-          const RouletteUnit.text("B", color: Colors.green),
-          const RouletteUnit.text("C", color: Colors.blue),
-          const RouletteUnit.text("D", color: Colors.yellow),
-        ]),
-        vsync: this);
-    super.initState();
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ルーレット'),
-      ),
-      body: Container(
-        child: Center(
-            child: GestureDetector(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  SizedBox(
-                    width: 260,
-                    height: 260,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 30),
-                      // Roulettのcontrollerを設定
-                      child: Roulette(
-                        controller: _controller,
-                        style: const RouletteStyle(
-                          dividerThickness: 4,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Icon(
-                    FontAwesomeIcons.longArrowAltDown,
-                    size: 45,
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-            ],
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton(
+            child: Text("次へ"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RoulettePage()),
+              );
+            },
           ),
-          onTap: () => _controller.rollTo(
-            2,
-            clockwise: true,
-            // TODO: ランダム値を特定のアクションで操作できるようにする
-            offset: Random().nextDouble(),
-          ),
-        )),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+        ));
   }
 }
