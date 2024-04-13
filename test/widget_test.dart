72b3bc49d68d31386e227485208e3b0dddc87417
rui-watanabe <wataru610806@gmail.com>
2024-04-13 14:48:28 +0900

    widget testの追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,22 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_roulette_app/main.dart';
+import 'package:flutter_roulette_app/view/roulette.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      ProviderScope(
+        child: const MaterialApp(
+          home: MyApp(),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('ルーレット'), findsOneWidget);
   });
 }
